16e44084f9e2d84997c54b31ede102603fd177fa
Herbert-C-byte <epsylon344@gmail.com>
2025-10-27 16:10:48 +0100

    Dino cranking

--- src/Dino.tsx
+++ src/Dino.tsx
@@ -3,23 +3,54 @@ import { useState, useEffect, useRef } from 'react';
 export default function Dino() {
   const [position, setPosition] = useState(0);
   const [isJumping, setIsJumping] = useState(false);
+  const isJumpingRef = useRef(false);
   const velocity = useRef(0);
-  const gravity = 1;
-  const jumpforce = 15;
+  const gravity = -1.2;
+  const jumpForce = 18;
   const groundLevel = 0;
   const maxHeight = 80;
 
-  useEffect(() => {
+   useEffect(() => {
+    isJumpingRef.current = isJumping;
+  }, [isJumping]);
+  
+  //handling the jump
+   useEffect(() => {
     const handleJump = (e: KeyboardEvent) => {
-      if (e.code === 'Space' && !isJumping) {
+      if (e.code === 'Space' && !isJumpingRef.current) {
+        isJumpingRef.current = true;
         setIsJumping(true);
-        velocity.current =  jumpforce;
+        velocity.current = jumpForce; // instant jump start
       }
-    }
+    };
 
     window.addEventListener('keydown', handleJump)
     return () => window.removeEventListener('keydown', handleJump)
   }, [isJumping]);
+  
+  
+  //game loop
+  useEffect(() => {
+    const gameLoop = setInterval(() => {
+      setPosition(previousPosition => {
+        let next = previousPosition + velocity.current
+        velocity.current += gravity
+
+        if (next <= groundLevel) {
+          next = groundLevel;
+          velocity.current = 0;
+          setIsJumping(false)
+        } else if (next >= maxHeight) {
+          next = maxHeight;
+          velocity.current = gravity;
+        }
+
+        return next;
+      })
+    }, 16)
+
+    return () => clearInterval(gameLoop);
+  }, []);
 
   return (
     <div 
