fd2a2bf1ea9c04695de0616c1766a9ad4ee8e520
Herbert-C-byte <epsylon344@gmail.com>
2025-10-27 10:10:16 +0100

    Cactus logic

--- src/Cactus.tsx
+++ src/Cactus.tsx
@@ -3,9 +3,19 @@ import { useState, useEffect } from 'react';
 export default function Cactus() {
   const [position, setPosition] = useState(500);
 
+  useEffect(() => {
+    const speed = 5;
+    const interval = setInterval(() => {
+      setPosition(previousPosition => (previousPosition <= 50 ? 500 : previousPosition - speed))}, 30)
+    return () => clearInterval(interval)
+  }, [])
+
+
   return (
-    <div className="cactus">
-     <img src="src/assets/cactus-idle.png" alt="Cactus" />
+    <div 
+    className="absolute bottom-0" 
+    style={{ left: `${position}px` }}>
+      <img src="src/assets/cactus-idle.png" alt="Cactus" className="w-8"/>
     </div>
   );
 }
