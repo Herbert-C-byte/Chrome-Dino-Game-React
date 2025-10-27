72550f2bb86d101b07551248d692a527812526ac
Herbert-C-byte <epsylon344@gmail.com>
2025-10-27 12:29:22 +0100

    Dino first logic

--- src/Dino.tsx
+++ src/Dino.tsx
@@ -1,17 +1,31 @@
-import { useState, useEffect, use } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 export default function Dino() {
   const [position, setPosition] = useState(0);
   const [isJumping, setIsJumping] = useState(false);
+  const velocity = useRef(0);
+  const gravity = 1;
+  const jumpforce = 15;
+  const groundLevel = 0;
+  const maxHeight = 80;
 
   useEffect(() => {
-    
-    
-  }, []);
+    const handleJump = (e: KeyboardEvent) => {
+      if (e.code === 'Space' && !isJumping) {
+        setIsJumping(true);
+        velocity.current =  jumpforce;
+      }
+    }
+
+    window.addEventListener('keydown', handleJump)
+    return () => window.removeEventListener('keydown', handleJump)
+  }, [isJumping]);
 
   return (
-    <div className="dino">
-    <img src="src/assets/dino-idle.png" alt="Dino Idle" />
+    <div 
+      className="absolute left-10" 
+      style={{bottom: `${position}px` }}>
+    <img src="src/assets/dino-idle.png" alt="Dino Idle" className='w-12'/>
     </div>
   );
 }
